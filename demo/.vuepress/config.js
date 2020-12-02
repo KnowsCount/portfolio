8a858fa168a34359c8856f3f2a79e7f50eb27bb7
KnowsCount <knowscount@gmail.com>
2020-12-02 20:38:19 +0800

    alpha.02122020@titleFix

--- demo/.vuepress/config.js
+++ demo/.vuepress/config.js
@@ -3,13 +3,13 @@
  * @Date: 2020-03-19 09:00:26
  * @Author: KnowsCount
  * @Github: https://github.com/KnowsCount/NKCS-Storage
- * @LastEditTime: 2020-12-02 10:42:28
+ * @LastEditTime: 2020-12-02 20:37:51
  * @FilePath: /vuepress-theme-terminal-master/demo/.vuepress/config.js
  */
 module.exports = {
 	dest: "docs",
 	theme: "terminal",
 	serviceWorker: true,
-	title: "KcNotes",
+	title: "KnowsCount's Portfolio",
 	base: "/",
 };
