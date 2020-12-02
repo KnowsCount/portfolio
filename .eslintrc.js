ba1713931880d870bc54f1d9a230e94ed668c4ef
KnowsCount <knowscount@gmail.com>
2020-12-02 23:25:05 +0800

    alpha.021220202@annotationMisc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
+/*
+ * @Description: My personal portfolio. 我的个人主页。
+ * @Date: 2020-03-19 09:00:26
+ * @Author: KnowsCount
+ * @Github: https://github.com/KnowsCount/NKCS-Storage
+ * @LastEditTime: 2020-12-02 18:52:06
+ * @FilePath: /vuepress-theme-terminal-master/.eslintrc.js
+ */
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ["plugin:vue/essential", "@vue/standard"],
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+	},
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+};
--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,13 @@
+/*
+ * @Description: My personal portfolio. 我的个人主页。
+ * @Date: 2020-03-19 09:00:26
+ * @Author: KnowsCount
+ * @Github: https://github.com/KnowsCount/NKCS-Storage
+ * @LastEditTime: 2020-12-02 18:51:01
+ * @FilePath: /vuepress-theme-terminal-master/.postcssrc.js
+ */
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
-}
+	plugins: {
+		autoprefixer: {},
+	},
+};
